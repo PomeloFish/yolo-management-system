5b56af6855415c3a209341e1224f005a74ba8f66
PomeloFish <202330690401@mail.scut.edu.cn>
2026-01-07 21:32:46 +0800

    Fix(types): modify properties of new interfaces to optional

--- src/types/views.ts
+++ src/types/views.ts
@@ -6,7 +6,7 @@ export interface Work{
     a_id?: number,
 
     i_file?: string,
-    annotated_at: Date,
+    annotated_at?: Date,
 
     l_class?: number,
     height?: number,
@@ -16,21 +16,21 @@ export interface Work{
 };
 
 export interface Work_count{
-    a_id: number,
-    a_name: string,
+    a_id?: number,
+    a_name?: string,
     
-    file_count: number,
-    label_count: number
+    file_count?: number,
+    label_count?: number
 };
 
 export interface Instance extends Class{
-    image: string,
-    height: number,
-    width: number,
-    center_x: number,
-    center_y: number
+    image?: string,
+    height?: number,
+    width?: number,
+    center_x?: number,
+    center_y?: number
 };
 
 export interface Instance_count extends Class{
-    i_count: number
+    i_count?: number
 }
